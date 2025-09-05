53e6bc12bb2580819fae13f18163c21a40ede1bf
LDSF <slash902@hotmail.com>
2025-09-05 13:38:21 -0600

    Add more examples to mock

--- tests/mocks/example-mocks.spec.ts
+++ tests/mocks/example-mocks.spec.ts
@@ -14,3 +14,21 @@ test("mocks a fruit and doesn't call api", async ({ page }) => {
   await expect(page.getByText('Strawberry')).toBeVisible();
   await page.pause();
 });
+
+test('gets the json from api and adds a new fruit', async ({ page }) => {
+  // Get the response and add to it
+  await page.route('*/**/api/v1/fruits', async route => {
+    const response = await route.fetch();
+    const json = await response.json();
+    json.push({ name: 'Loquat', id: 100 });
+    // Fulfill using the original response, while patching the response body
+    // with the given JSON object.
+    await route.fulfill({ response, json });
+  });
+
+  // Go to the page
+  await page.goto('https://demo.playwright.dev/api-mocking');
+
+  // Assert that the new fruit is visible
+  await expect(page.getByText('Loquat', { exact: true })).toBeVisible();
+});
